40dd61f3841b6bc5da1fc9aea0e740538ad7acc4
Katy9912 <55024502+Katy9912@users.noreply.github.com>
2020-10-12 19:46:15 -0500

    Actualización Front-end- lógica

---
+++ GraphicsMenu.py
@@ -0,0 +1,163 @@
+#Se importan las librerias requeridas para la ejecución de la clase
+import pandas as pd
+import numpy as np
+import os
+from PyQt5.QtWidgets import *
+from PyQt5 import QtWidgets
+
+# Se importa el .py de la interfaz gráfica
+from Scripts.App_V2.Graficas_menu import *
+
+from Scripts.App_V2.GraphicsComparison import *
+from Scripts.App_V2.Graphic_Data import *
+from Scripts.App_V2.GraphicsView import *
+
+
+class GraphicsMenu(QtWidgets.QMainWindow):
+    def __init__(self,parent):
+        super(GraphicsMenu,self).__init__()
+
+        #Esta variable guarda referencia a la interfaz "parent" de esta interfaz
+        # Es necesaria para la interaccion de los botones 
+        self.parent = parent
+
+        self.ui = Ui_graphics_menu()
+        self.ui.setupUi(self)
+        
+        #Estas dos lineas siguientes bloquean el boton cerrar de la ventana
+        self.setWindowFlags(self.windowFlags() | QtCore.Qt.CustomizeWindowHint)
+        self.setWindowFlags(self.windowFlags() & ~QtCore.Qt.WindowCloseButtonHint)
+        
+        #Se muestra la pantalla (interfaz)
+        self.show()
+
+        #Lista con el nombre de las categorias que actualmente estan definidas
+        self.botones = ['PF','HRMA_y','HRMB_y','HRMC_y','I','I0_B','PLTA','PSTA','V','W']
+        
+        
+        self.info = None #Estructura que recibe los datos del dataframe
+        self.headers = [None] #Lista para almacenar los headers del dataframe
+        self.variables = [] #Lista que guarda todas las variables de las categorias existentes 
+        self.exists = False #Variable que ayuda a saber si se llama a la ventana para ver la grafica generada
+
+        # Listeners de los botones de la interfaz
+        self.ui.PF.clicked.connect(self.graficar)
+        self.ui.HRMA_y.clicked.connect(self.graficar)
+        self.ui.HRMB_y.clicked.connect(self.graficar)
+        self.ui.HRMC_y.clicked.connect(self.graficar)
+        self.ui.I.clicked.connect(self.graficar)
+        self.ui.I0_B.clicked.connect(self.graficar)
+        self.ui.PLTA.clicked.connect(self.graficar)
+        self.ui.PSTA.clicked.connect(self.graficar)
+        self.ui.V.clicked.connect(self.graficar)
+        self.ui.W.clicked.connect(self.graficar)
+
+        #El boton "Perzonalizado..." se coloca activo
+        self.ui.custom_button.setStyleSheet("border-color: black") 
+        self.ui.custom_button.setEnabled(True)
+        self.ui.custom_button.clicked.connect(self.custom)
+        
+        self.ui.backButton.clicked.connect (self.back)
+        
+        
+        
+
+    
+    #Metodo que recibe la informacion del merge para saber que graficas estarán disponibles y habilitarlas
+    def cargarData(self,data):
+        self.info = data
+
+    # Metodo para obtener los headers de las columnas del dataframe
+    def recuperarColumnas(self):
+        self.headers = self.info.columns.tolist()
+
+
+    # Metodo que evalua que graficas estaran disponibles
+    def comparar(self):
+        self.recuperarColumnas()
+
+        for key in self.botones:
+            if key in self.headers:
+                self.variables.append(key)
+                self.headers.remove(key)
+        self.categoriasHabilitadas()
+
+    #Metodo que se encarga de habilitar las categorias de graficas disponibles
+    def categoriasHabilitadas(self):
+        for value in self.variables:
+            if value == "PF":
+                self.ui.PF.setStyleSheet("border-color: black")
+                self.ui.PF.setEnabled(True)
+            if value == "HRMA_y":
+                self.ui.HRMA_y.setStyleSheet("border-color: black")
+                self.ui.HRMA_y.setEnabled(True)
+            if value =="HRMB_y":
+                self.ui.HRMB_y.setStyleSheet("border-color: black")
+                self.ui.HRMB_y.setEnabled(True)
+            if value == "HRMC_y":
+                self.ui.HRMC_y.setStyleSheet("border-color: black")
+                self.ui.HRMC_y.setEnabled(True)
+            if value == "V":
+                self.ui.V.setStyleSheet("border-color: black")
+                self.ui.V.setEnabled(True)
+            if value == "W":
+                self.ui.W.setStyleSheet("border-color: black")
+                self.ui.W.setEnabled(True)
+            if value == "I":
+                self.ui.I.setStyleSheet("border-color: black")
+                self.ui.I.setEnabled(True)
+            if value == "PSTA":
+                self.ui.PSTA.setStyleSheet("border-color: black")
+                self.ui.PSTA.setEnabled(True)
+            if value == "PLTA":
+                self.ui.PLTA.setStyleSheet("border-color: black")
+                self.ui.PLTA.setEnabled(True)
+            if value == "I0_B":
+                self.ui.I0_B.setStyleSheet("border-color: black")
+                self.ui.I0_B.setEnabled(True)
+            
+    #Metodo que llama a la ventana del boton "Personalizado..."
+    def custom(self):
+        #En el constructor de la clase se pasan como argumento: Esta clase como "parent" y
+        # La lista de headers como "variables"
+        self.custom = GraphicsComparison(parent=self,variables=self.headers)
+        
+        #Se pasa el dataframe generado para graficar
+        self.custom.setData(data=self.info)
+        self.custom.show()
+    
+    #Metodo que llama a la interfaz para ver las grafica generada de la categoria seleccionada
+    def graficar(self):
+        self.view = GraphicsView(parent=self) #En el constructor se pasa esta clase como "parent"
+        self.exists = True
+        self.view.setData(datos=self.info) #Se envia como argumento: dataframe como "info"
+
+        #Se envia como argumento: El nombre del boton clickeado para saber qué categoria se va agraficar 
+        self.view.setName(name=self.sender().objectName())
+        
+        self.view.plotitem() #Se llama al metodo que genera la grafica
+        self.view.show() #Se muestra la interfaz con la visualizacion de la grafica
+
+    # Metodo del boton "Regresar"
+    def back(self):
+        #Cierra la interfaz actual
+        self.close()
+        #Muestra la interfaz padre (Menu Principal)
+        self.parent.raise_()
+
+
+
+'''if __name__ == "__main__":
+    import sys, time, os
+    from PyQt5.QtWidgets import QFileDialog, QMessageBox, QWidget
+    from PyQt5.QtWebEngineWidgets import *
+    from PyQt5.QtWidgets import *
+    from PyQt5 import QtWidgets
+    from PyQt5.QtCore import *
+
+    app = QtWidgets.QApplication(sys.argv)
+    my_app = GraphicsMenu()
+    my_app.setData(data="Hola")
+    #print("Data")
+    #print(my_app.getData())
+    sys.exit(app.exec_())'''
---
+++ GraphicsView.py
@@ -0,0 +1,88 @@
+import pandas as pd
+import os
+
+from PyQt5.QtWidgets import QFileDialog, QMessageBox, QWidget
+from PyQt5.QtWebEngineWidgets import *
+from PyQt5.QtWidgets import *
+from PyQt5 import QtWidgets
+from PyQt5.QtCore import *
+
+from Graficas_Window import *
+from Graphic_Data import * 
+
+from Scripts.App_V2.Graficas_menu import *
+from Scripts.App_V2.Graficas_Window import *
+from Scripts.App_V2.Graphic_Data import * 
+
+class GraphicsView (QtWidgets.QMainWindow):
+    def __init__(self,parent):
+        super(GraphicsView, self).__init__()
+        self.ui = Ui_MainWindow()
+        
+        #Esta variable guarda referencia a la interfaz "parent" de esta interfaz
+        # Es necesaria para la interaccion de los botones 
+        self.parent = parent
+        
+        self.ui.setupUi(self)
+        
+        #Estas dos lineas siguientes bloquean el boton cerrar de la ventana
+        self.setWindowFlags(self.windowFlags() | QtCore.Qt.CustomizeWindowHint)
+        self.setWindowFlags(self.windowFlags() & ~QtCore.Qt.WindowCloseButtonHint)
+
+        #Se muestra la pantalla (interfaz)
+        self.show()
+
+        self.init_dir = "../" #Esta variable hace referencia al directorio raiz donde se guardaran las graficas
+        self.info = None #Esta variable almacena la informacion del dataframe que se usa para graficar
+        self.key = "" #Esta variable guarda el nombre de la "variable" que se quiere graficar
+        self.plot_list = [] #Esta variable almacena una lista de los archivos html generados
+
+        #Listeners de los botones "Regresar" e "Inicio"
+        self.ui.backButton.clicked.connect(self.back)
+        self.ui.homeButton.clicked.connect(self.home)
+
+        #Se inicializa la vista Web para las graficas y la logica del programa
+        self.web = QWebEngineView(self.ui.widget)
+        self.web.resize(1040,500)
+        self.web.setZoomFactor(0.8)
+
+        self.data = Graphic_Data()
+
+    #Metodo que obtiene el dataframe para generar las graficas
+    def setData(self,datos):
+        self.info = datos
+    #Metodo que obtiene el nombre de la categoria que se graficara (Nombre del boton)
+    def setName(self,name):
+        self.key = name
+
+    #Metodo que genera la grafica
+    def plotitem(self):
+        plot_name = self.data.plot(dataframe=self.info, key=self.key)
+        self.plot_list.append(plot_name)
+        plot_file = os.path.join("file:///" + self.init_dir, plot_name)
+        self.web.load(QUrl(plot_file))
+        self.web.show()
+    
+    # Metodo del boton "Regresar"
+    def back(self):
+        #Cierra la interfaz actual
+        self.close()
+        #Muestra la interfaz padre (Menu)
+        self.parent.raise_()
+    
+    #Metodo del boton "Inicio"
+    def home(self):
+        #Cierra interfaz actual
+        self.close()
+        #Cierra la interfaz padre (Menu)
+        self.parent.close()
+        #Muestra la interfaz padre.padre(Menu Principal)
+        self.parent.parent.raise_()
+        
+    #Metodo que elimina los archivos html generados al momento de cerrar la ventana
+    def closeEvent(self, event):
+        if event:
+            if self.plot_list:
+                for file in self.plot_list:
+                    os.remove(file)
+                    self.plot_list.remove(file)
